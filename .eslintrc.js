0a52897748f7335da010c449576014798e74e377
kerasus <aliesmaeeli20@gmail.com>
2022-09-18 12:39:59 +0430

    - update version

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "airbnb-base",
+  ],
+  parserOptions: {
+    ecmaVersion: "latest",
+    parser: "@typescript-eslint/parser",
+    sourceType: "module",
+  },
+  plugins: [
+    "vue",
+    "@typescript-eslint",
+  ],
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    semi: ["error", "never"],
+    quotes: ["error", "single"],
+  },
+};
