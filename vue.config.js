3727264128673d96f871caba235f4a42ca166d5a
JasGH <yasaman.gonehi@gmail.com>
2022-12-19 19:14:03 +0330

    lint fix

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 module.exports = {
-    pages: {
-        index: {
-            entry: 'src/main.ts'
-        }
+  pages: {
+    index: {
+      entry: 'src/main.ts',
     },
-    devServer: {
-        proxy: {
-            '^/api': {
-                target: 'https://3a.alaatv.com',
-                changeOrigin: true
-            },
-        }
-    }
+  },
+  devServer: {
+    proxy: {
+      '^/api': {
+        target: 'https://3a.alaatv.com',
+        changeOrigin: true,
+      },
+    },
+  },
 }
