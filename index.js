811ccb9c4db43befc6fb04df0d83acb1984739a8
JasGH <yasaman.gonehi@gmail.com>
2022-12-31 17:42:45 +0330

    export update

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const VueTiptapKatex = require('./src/vue3-tiptap-katex.vue')
+const VueTiptapKatex = require('./src/vue3-tiptap-katex.vue').default
 
 module.exports = VueTiptapKatex
 module.exports.default = VueTiptapKatex
