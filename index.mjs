2a7948d57d33059e3e622ffdfd0b765c28a15783
JasGH <yasaman.gonehi@gmail.com>
2023-05-28 19:52:30 +0330

    index file added

---
+++ index.mjs
@@ -0,0 +1,13 @@
+'use strict'
+
+const VueTiptapKatex = require('./src/vue3-tiptap-katex.vue').default
+
+module.exports = VueTiptapKatex
+module.exports.default = VueTiptapKatex
+import VueTiptapKatex from './src/vue3-tiptap-katex.vue'
+const VueTiptapKatexObject = { VueTiptapKatex }
+
+export {
+    VueTiptapKatexObject as default,
+    VueTiptapKatex
+}
